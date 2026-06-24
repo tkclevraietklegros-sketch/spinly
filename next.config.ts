df26ec83498a73344af3e7ffcd397aee07003173
tkclevraietklegros-sketch <tkclevraietklegros@gmail.com>
2026-06-24 11:27:31 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "screen-capture=(self)",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
